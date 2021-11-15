56a0d4be53a3ea34bedb38cdbd05eba4cd7c13f9
Zero-slayer <71024460+Zero-slayer@users.noreply.github.com>
2021-11-15 14:20:59 +0100

    Updated to use the register endpoint from backend

--- src/App.jsx
+++ src/App.jsx
@@ -26,6 +26,31 @@ function LogIn({ login }) {
 
 }
 
+function RegisterUser({ user }) {
+  const init = { username: "", password: "" };
+  const [newUserCredentials, setNewUserCredentials] = useState(init);
+
+  const performRegister = (event) => {
+    event.preventDefault();
+    user(newUserCredentials.username, newUserCredentials.password);
+  }
+  const onChange = (event) => {
+    setNewUserCredentials({ ...newUserCredentials, [event.target.id]: event.target.value })
+  }
+
+  return (
+    <div>
+      <h2>Login</h2>
+      <form onChange={onChange}>
+        <input type="text" placeholder="New user name" id="username" />
+        <input type="text" placeholder="New password" id="password" />
+        <button onClick={performRegister}>Register</button>
+      </form>
+    </div>
+  )
+
+}
+
 function LoggedIn() {
   const [dataFromServer, setDataFromServer] = useState("Loading...");
 
@@ -53,9 +78,14 @@ function App() {
     facade.login(user, pass)
       .then(response => setLoggedIn(true));
   }
+  const newUser = (user, pass) => {
+    facade.register(user, pass);
+  }
 
   return (
     <div>
+      <p>Here's the frontend of what Albert aka aj419 has created with the help of the template created by group B5</p>
+      {!loggedIn ? (<RegisterUser user={newUser} />) : null}
       {!loggedIn ? (<LogIn login={login} />) : 
       (<div>
         <LoggedIn />
